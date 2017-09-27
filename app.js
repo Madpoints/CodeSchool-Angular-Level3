56b56392b3245d2a2978088fbedd866130046f75
John Furtado <jf8846@gmail.com>
2017-09-27 21:57:41 +0000

    using directives to manipulate the DOM

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
     var app = angular.module('store', []);
     
     app.controller('StoreController', function() {
-        this.product = gem;    
+        this.products = gems;    
     });
-    
-    var gem = {
+    // Gems as an array of objects
+    var gems = [{
         name: 'Dodecahedron',
         price: 2.95,
-        description: '...'
-    }
+        description: 'Fancy',
+        canPurchase: true,
+        soldOut: false
+    },{
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'Five sides',
+        canPurchase: true,
+        soldOut: false        
+    }];
 })();    
