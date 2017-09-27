69f5a9f50e918eeda71dea9cb31091ee75181e39
John Furtado <jf8846@gmail.com>
2017-09-27 23:14:55 +0000

    displaying tabs using angular and controllers

--- app.js
+++ app.js
@@ -4,6 +4,17 @@
     app.controller('StoreController', function() {
         this.products = gems;    
     });
+    
+    app.controller('PanelController', function() {
+        this.tab = 1;
+        
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
     // Gems as an array of objects
     var gems = [{
         name: 'Dodecahedron',
